31fe5a8c5b2e7a8be4f766f0d7ce947ab5f86320
Vasiliy Kudryavtsev <kudv@1c.ru>
2016-11-02 09:52:55 +0300

    Добавил разметку arrange-act-assert

--- lists/MyVectorTests.cpp
+++ lists/MyVectorTests.cpp
@@ -3,29 +3,42 @@
 #include "MyVectorTests.h"
 
 void test_vector_capacity() {
+	// Arrange
 	MyVector mvector;
+
+	// Act
 	assert_equals(0, mvector.size());
 	for (int i = 0; i < 11; i++) {
 		mvector.push_back(i);
 	}
+
+	// Assert
 	assert_equals(20, mvector.capacity());
 }
 
 void test_vector_push_back() {
+	// Arrange
 	MyVector mvector;
+	
+	// Act
 	for (int i = 0; i < 10; i++) {
 		mvector.push_back(i);
 	}
+
+	// Assert
 	assert_equals(9, mvector[9]);
 }
 
 void test_vector_index_access_throws_on_too_big_index() {
+	
+	// Arrange
 	MyVector mvector;
 	for (int i = 0; i < 10; i++) {
 		mvector.push_back(i);
 	}
 	bool wasException = false;
 
+	// Act
 	try {
 		int el = mvector[mvector.size() + 1];
 	}
@@ -34,16 +47,23 @@ void test_vector_index_access_throws_on_too_big_index() {
 		wasException = true;
 	}
 
+	// Assert
 	assert_equals(true, wasException);
 
 }
 
 void test_vector_pop_back() {
+
+	// Arrange
 	MyVector mvector;
 	for (int i = 0; i < 10; i++) {
 		mvector.push_back(i);
 	}
+
+	// Act
 	mvector.pop_back();
+
+	// Assert
 	assert_equals(9, mvector.size());
 }
 
